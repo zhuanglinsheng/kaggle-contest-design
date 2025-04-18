5933c890a29a649b2734eb577b607b219169f4f0
zhuanglinsheng <zhuanglinsheng@outlook.com>
2025-04-18 16:13:06 +0800

    create-`utils.py`
    Add type `TableName`

---
+++ metakaggle/utils.py
@@ -0,0 +1,55 @@
+from typing import Literal
+
+type TableName = Literal[
+	'Submissions',
+
+	'Kernels',
+	'KernelVotes',
+	'KernelTags',
+	'KernelAcceleratorTypes',
+	'KernelLanguages',
+	'KernelVersions',
+	'KernelVersionDatasetSources',
+	'KernelVersionKernelSources',
+	'KernelVersionCompetitionSources',
+	'KernelVersionModelSources',
+
+	'Models',
+	'ModelVersions',
+	'ModelTags',
+	'ModelVotes',
+	'ModelVariations',
+	'ModelVariationVersions',
+
+	'Users',
+	'UserOrganizations',
+	'UserAchievements',
+	'UserFollowers',
+
+	'Teams',
+	'TeamMemberships',
+
+	'Organizations',
+
+	'Competitions',
+	'CompetitionTags'
+
+	'Forums',
+	'ForumTopics',
+	'ForumMessages',
+	'ForumMessageVotes',
+	'ForumMessageReactions',
+
+	'Datasets',
+	'DatasetVersions',
+	'DatasetVotes',
+	'DatasetTags',
+	'DatasetTasks',
+	'DatasetTaskSubmissions',
+	'Datasources',
+
+	'Tags',
+
+	'Episodes',
+	'EpisodeAgents',
+]
